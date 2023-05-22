04ac57067876f97db5dcb283a4fd7948e9cff74c
rakibul24466 <77064885+rakibul24466@users.noreply.github.com>
2023-05-23 02:11:40 +0600

    Project Setup

---
+++ FCRS_Project/Course_Registration_portal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CourseRegistrationPortalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Course_Registration_portal'
