024d057c17726add1d1864a9bb1224583a82252f
rakibul24466 <77064885+rakibul24466@users.noreply.github.com>
2023-05-23 02:27:07 +0600

    Revert "Project Setup"
    This reverts commit 04ac57067876f97db5dcb283a4fd7948e9cff74c.

--- FCRS_Project/Course_Registration_portal/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CourseRegistrationPortalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Course_Registration_portal'
